ed319b4d3a1b190eb2a575fb9ac9f6c2e3620bf4
deadpanther <deadpanther@github.com>
2022-05-11 02:36:36 -0400

    Completed AWS Cognito Setup

---
+++ s3/js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_vBajLIEsh', // e.g., us-east-2_uXboG5pAb
+        userPoolClientId: 'sgs8bt386138f9k4rsn702s85', // e.g., 25ddkmj4v6hfsfvruhp3848y7oi
+        region: 'us-east-1' // e.g., us-east-2
+    },
+};
